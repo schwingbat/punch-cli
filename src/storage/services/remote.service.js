2d3ddcec46e469cd5758d2161a3d089d49600c96
Tony McCoy <tony@ratwizard.io>
2020-04-18 22:40:24 -0700

    Fixed web input colors in light mode, updated project and punch details styling to use a shared table component

---
+++ src/storage/services/remote.service.js
@@ -0,0 +1,38 @@
+/**
+ * Uses a remote Punch instance as a storage backend.
+ */
+
+module.exports = function (config, Punch) {
+  return {
+    name: "template",
+
+    // Saves a single punch object
+    async save(punch) {},
+
+    // Returns the currently running punch (or null)
+    // Optionally filters by project
+    async current(project) {},
+
+    // Returns the most recent punch
+    // Optionally filters by project
+    async latest(project) {},
+
+    // Returns an array of punches for which the passed
+    // function returns true.
+    async filter(fn) {},
+
+    // Returns the first punch for which the passed
+    // function returns true.
+    async find(fn) {},
+
+    // Deletes a given punch
+    async delete(punch) {},
+
+    // Commits any pending changes
+    async commit() {},
+
+    // Called before the program exits.
+    // Close connections and do any necessary cleanup.
+    async cleanUp() {},
+  };
+};
