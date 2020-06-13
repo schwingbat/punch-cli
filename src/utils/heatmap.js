01c0ecea2e3dcf85e7a0181573bba67ca3e50db2
Tony McCoy <tony@ratwizard.io>
2020-06-13 13:48:08 -0700

    Removed heatmap.day - replaced with logging/day-graphic

--- src/utils/heatmap.js
+++ src/utils/heatmap.js
@@ -1,12 +1,11 @@
 /*
  * Takes an array of punches and returns a heatmap showing
- * which times of day you tend to work the most.
+ * which times of day/month you tend to work the most.
  *
  * There are a few different heatmap functions depending on
  * how you want to visualize the data.
  */
 
-const startOfDay = require("date-fns/startOfDay");
 const chalk = require("chalk");
 
 const zeroArray = (length) => {
@@ -43,56 +42,7 @@ const collapseToLevels = (segments) => {
   });
 };
 
-function day(punches, config, opts = {}) {
-  const segments = zeroArray(48);
-
-  // const mapLevels = [
-  //   chalk.grey("▊"), // 0
-  //   chalk.green("▊"), // 1
-  //   chalk.yellow("▊"), // 2
-  //   chalk.red("▊") // 3
-  // ];
-
-  const mapLevels = [
-    chalk.grey("▁"), // 0
-    chalk.green("▃"), // 1
-    chalk.yellow("▆ "), // 2
-    chalk.red("▉"), // 3
-  ];
-
-  // Add 1 to each 30 minute segment the punch overlaps with.
-  for (const punch of punches) {
-    const start = startOfDay(punch.in);
-    const first = punch.in.getTime() - start.getTime();
-    const last = (punch.out || new Date()).getTime() - start.getTime();
-
-    const f = Math.round((first / 1000 / 60 / 60) * 2);
-    const l = Math.round((last / 1000 / 60 / 60) * 2);
-
-    for (let i = f; i < l; i++) {
-      segments[i % 48] += 1;
-    }
-  }
-
-  const topRow = config.display.use24HourTime
-    ? "00. . . . . 06. . . . . 12. . . . . 18. . . . ."
-    : "12. . 3 . . 6 . . 9 . . 12. . 3 . . 6 . . 9 . .";
-
-  const map = collapseToLevels(segments)
-    .map((s) => mapLevels[s])
-    .join("");
-
-  return (
-    "".padEnd(opts.labelPadding || 3) +
-    topRow +
-    "\n" +
-    "HEATMAP".padEnd(opts.labelPadding || 3) +
-    map +
-    "\n"
-  );
-}
-
-function week(punches, config, opts = {}) {
+exports.week = function week(punches, config, opts = {}) {
   const segments = zeroArray(7);
 
   const mapLevels = [
@@ -126,15 +76,9 @@ function week(punches, config, opts = {}) {
     map +
     "\n"
   );
-}
+};
 
-function month(punches, config) {
+exports.month = function month(punches, config) {
   const segments = zeroArray(31);
   const topRow = "SU  MO  TU  WE  TH  FR  SA  ";
-}
-
-module.exports = {
-  day,
-  week,
-  month,
 };
