2252b76d32049a7507487c9f69e7ad6a4c3029c7
Tony McCoy <tony@ratwizard.io>
2020-06-12 23:26:53 -0700

    Converted storage services to functions instead of requiring them all at startup

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,12 +1,13 @@
 const services = {
-  ledger: require("./services/ledger.service"),
+  ledger: () => require("./services/ledger.service"),
+  sqlite: () => require("./services/sqlite.service"),
 };
 
 module.exports = function (config) {
   const storageName = config.storageType.toLowerCase();
 
   if (services[storageName]) {
-    return services[storageName].bind(null, config);
+    return services[storageName]().bind(null, config);
   } else {
     throw new Error(
       `Storage service \`${config.storageType}\` does not exist.`
