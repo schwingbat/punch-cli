c342409082474c62c10b21b7e484f4803e01f3c4
Tony McCoy <tony@ratwizard.io>
2020-06-12 22:40:10 -0700

    Minor improvement to appearance of weekly heatmap

--- src/utils/heatmap.js
+++ src/utils/heatmap.js
@@ -9,7 +9,7 @@
 const startOfDay = require("date-fns/startOfDay");
 const chalk = require("chalk");
 
-const zeroArray = length => {
+const zeroArray = (length) => {
   const arr = [];
   for (let i = 0; i < length; i++) {
     arr.push(0);
@@ -17,7 +17,7 @@ const zeroArray = length => {
   return arr;
 };
 
-const collapseToLevels = segments => {
+const collapseToLevels = (segments) => {
   // Get max value so we can clamp the segments into relative levels.
   let max = 0;
 
@@ -28,7 +28,7 @@ const collapseToLevels = segments => {
   }
 
   // Divide sums into four levels (0, 1, 2 and 3)
-  return segments.map(s => {
+  return segments.map((s) => {
     let val = (s / max) * 3;
 
     // If we have a fractional value, consider it level 1.
@@ -46,11 +46,18 @@ const collapseToLevels = segments => {
 function day(punches, config, opts = {}) {
   const segments = zeroArray(48);
 
+  // const mapLevels = [
+  //   chalk.grey("▊"), // 0
+  //   chalk.green("▊"), // 1
+  //   chalk.yellow("▊"), // 2
+  //   chalk.red("▊") // 3
+  // ];
+
   const mapLevels = [
-    chalk.grey("▊"), // 0
-    chalk.green("▊"), // 1
-    chalk.yellow("▊"), // 2
-    chalk.red("▊") // 3
+    chalk.grey("▁"), // 0
+    chalk.green("▃"), // 1
+    chalk.yellow("▆ "), // 2
+    chalk.red("▉"), // 3
   ];
 
   // Add 1 to each 30 minute segment the punch overlaps with.
@@ -72,7 +79,7 @@ function day(punches, config, opts = {}) {
     : "12. . 3 . . 6 . . 9 . . 12. . 3 . . 6 . . 9 . .";
 
   const map = collapseToLevels(segments)
-    .map(s => mapLevels[s])
+    .map((s) => mapLevels[s])
     .join("");
 
   return (
@@ -89,10 +96,10 @@ function week(punches, config, opts = {}) {
   const segments = zeroArray(7);
 
   const mapLevels = [
-    chalk.grey("███▊"),
-    chalk.green("███▊"),
-    chalk.yellow("███▊"),
-    chalk.red("███▊")
+    chalk.grey("▂▂▂▂"),
+    chalk.green("▄▄▄▄"),
+    chalk.yellow("▅▅▅▅"),
+    chalk.red("████"),
   ];
 
   for (const punch of punches) {
@@ -105,10 +112,10 @@ function week(punches, config, opts = {}) {
     }
   }
 
-  const topRow = "SU  MO  TU  WE  TH  FR  SA  ";
+  const topRow = " SU  MO  TU  WE  TH  FR  SA ";
 
   const map = collapseToLevels(segments)
-    .map(s => mapLevels[s])
+    .map((s) => mapLevels[s])
     .join("");
 
   return (
@@ -129,5 +136,5 @@ function month(punches, config) {
 module.exports = {
   day,
   week,
-  month
+  month,
 };
